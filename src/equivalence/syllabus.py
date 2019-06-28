833dca26e51ff144ec76b5b30b3f1fd650e14481
StanleySathler <stanleysathlerpinto@gmail.com>
2019-06-27 21:51:07 -0300

    refac(equivalence): make "syllabus" comparison return True

--- src/equivalence/syllabus.py
+++ src/equivalence/syllabus.py
@@ -6,15 +6,16 @@ from constants import columns
 
 def similarity(discipline_a, discipline_b):
     """
-    Calculate the percentage of similarity between the 
+    Calculate the percentage of similarity between the
     syllabus of both courses.
 
-    @todo: implement this algorithm using some Machine 
+    @todo: implement this algorithm using some Machine
     Learning technique.
     """
     a = discipline_a[columns.DISCIPLINE_SYLLABUS]
     b = discipline_b[columns.DISCIPLINE_SYLLABUS]
     words_a = a.split()
     words_b = b.split()
-    return (len(words_b) / len(words_a))
+    # return (len(words_b) / len(words_a))
+    return True
 

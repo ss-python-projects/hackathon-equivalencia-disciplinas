dd314a811f10d418f6fe0f06f4cc7c7b07b98132
StanleySathler <stanleysathlerpinto@gmail.com>
2019-06-28 09:56:08 -0300

    add(test): brain/test_syllabus.py

---
+++ test/brain/test_syllabus.py
@@ -0,0 +1,17 @@
+from brain.syllabus import Syllabus as SyllabusBrain
+from model.discipline import Discipline as DisciplineModel
+
+class TestSyllabus:
+    def test_calculate_similarity_equal_syllabus(self):
+        discipline_a = DisciplineModel("2458", "Discipline A", "80", "Programming Introduction")
+        discipline_b = DisciplineModel("2411", "Discipline B", "80", "Programming Introduction")
+        actual = SyllabusBrain(discipline_a, discipline_b).calculate_similarity()
+        expected = 1
+        assert actual == expected, "Similarity should be 1 for completely equal syllabus"
+
+    def test_calculate_similarity_slightly_different_syllabus(self):
+        discipline_a = DisciplineModel("2458", "Discipline A", "80", "Programming Introduction for Beginners")
+        discipline_b = DisciplineModel("2411", "Discipline B", "80", "Programming Introduction")
+        actual = SyllabusBrain(discipline_a, discipline_b).calculate_similarity()
+        expected = 0.6546536707079772
+        assert actual == expected, "Similarity should be ~= 0.65 for slightly different syllabus" # not a cool result - I'd expect at least 80% :(

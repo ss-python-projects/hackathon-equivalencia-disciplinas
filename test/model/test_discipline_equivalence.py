14094750d5fe5a0798438726f3b0bc22894f78be
StanleySathler <stanleysathlerpinto@gmail.com>
2019-06-29 00:20:01 -0300

    fix(test): "test_discipline_equivalence" now considers UUID

--- test/model/test_discipline_equivalence.py
+++ test/model/test_discipline_equivalence.py
@@ -7,14 +7,17 @@ class TestDisciplineEquivalence():
         discipline = Discipline("2428", "Discipline A", "40", "Syllabus A")
         equivalence = Discipline("5050", "Discipline B", "35", "Syllabus B")
         discipline_equivalence = DisciplineEquivalence(discipline, equivalence)
+        discipline_equivalence.uuid = "1234"
         actual = discipline_equivalence.serialize()
         expected = pd.DataFrame(
             columns=[
+                "UUID",
                 "COD_DISCIP_ORIG", "NOME_DISCIP_ORIG", "CARGAH_DISCIP_ORIG", "EMENTA_DISCIP_ORIG",
                 "COD_DISCIP_EQUIV", "NOME_DISCIP_EQUIV", "CARGAH_DISCIP_EQUIV", "EMENTA_DISCIP_EQUIV",
                 "TIPO_EQUIV",
             ],
             data = [[
+                "1234",
                 discipline.id, discipline.name, discipline.workload, discipline.syllabus,
                 equivalence.id, equivalence.name, equivalence.workload, equivalence.syllabus,
                 1,

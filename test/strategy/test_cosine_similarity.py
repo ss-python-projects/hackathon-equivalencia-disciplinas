a4baf2ae59f0e91746870bd17964204b5a7d78e9
StanleySathler <stanleysathlerpinto@gmail.com>
2019-06-29 02:01:49 -0300

    add(test/strategy): more test cases

--- test/strategy/test_cosine_similarity.py
+++ test/strategy/test_cosine_similarity.py
@@ -1,25 +1,11 @@
 from strategy.cosine_similarity import CosineSimilarity as CosineSimilarityStrategy
 
 class TestCosineSimilarity:
-    def test_compare_with_equal_sentences(self):
-        strategy = CosineSimilarityStrategy()
-        expected = 1
-        actual = strategy.compare(
-            "Aplicações executadas em dispositivos móveis",
-            "Aplicações executadas em dispositivos móveis"
-        )
-        
-        assert actual == expected
-
-    def test_compare_with_slightly_different_sentences(self):
-        strategy = CosineSimilarityStrategy()
-        expected = 0.3779644730092272
-        actual = strategy.compare(
-            "Aplicações executadas em dispositivos móveis",
-            "Programação para dispositivos móveis"
-        )
-        
-        assert actual == expected
+    """
+    Currently, these tests are more a way of testing the 
+    strategies' efficiency than testing their behavior 
+    itself.
+    """
 
     def test_compare_slightly_different_sentences_and_many_words(self):
         strategy = CosineSimilarityStrategy()
@@ -30,3 +16,24 @@ class TestCosineSimilarity:
         )
         
         assert actual == expected
+
+    def test_compare_real_equivalence_1(self):
+        strategy = CosineSimilarityStrategy()
+        expected = 0.5050505050505051
+        actual = strategy.compare(
+            "Desenvolvimento de algoritmos e estruturas de dados básicas. Desenvolvimento de programação em uma linguagem de alto nível. Metodologia de desenvolvimento de programas: refinamentos sucessivos, modularização e testes básicos",
+            "Paradigmas de programação. Lógica de programação.Introdução ao conceito e uso de algoritmos. Estruturas condicionais e de seleção. Estrutura de repetição. Vetores e matrizes. Formas de representação dos algoritmos"
+        )
+        
+        assert actual == expected
+
+    def test_compare_real_equivalence_2(self):
+        strategy = CosineSimilarityStrategy()
+        expected = 0.342211095283218
+        actual = strategy.compare(
+            "Sistemas de Coordenadas Tridimensionais. Superfícies Cilíndricas e Quádricas. Coordenadas Cilíndricas e Esféricas. Funções Vetoriais. Funções de várias Variáveis. Derivadas Parciais. Plano Tangente. Regra da cadeia. Derivada Direcional e Vetor Gradie",
+            "Funções Vetoriais. Funções de várias variáveis. Integrais múltiplas. Derivadas de funções vetoriais. Operadores diferenciais e aplicações. Equações diferenciais. Modelagem. Métodos para solução equações diferenciais ordinárias lineares e não lineares"
+        )
+        
+        assert actual == expected
+        
--- test/strategy/test_sequence_matcher.py
+++ test/strategy/test_sequence_matcher.py
@@ -1,32 +1,32 @@
 from strategy.sequence_matcher import SequenceMatcher as SequenceMatcherStrategy
 
 class TestSequenceMatcher:
-    def test_compare_with_equal_sentences(self):
-        strategy = SequenceMatcherStrategy()
-        expected = 1
-        actual = strategy.compare(
-            "Aplicações executadas em dispositivos móveis",
-            "Aplicações executadas em dispositivos móveis"
-        )
-        
-        assert actual == expected
-
-    def test_compare_with_slightly_different_sentences(self):
-        strategy = SequenceMatcherStrategy()
-        expected = 0.625
-        actual = strategy.compare(
-            "Aplicações executadas em dispositivos móveis",
-            "Programação para dispositivos móveis"
-        )
-        
-        assert actual == expected
-
     def test_compare_slightly_different_sentences_and_many_words(self):
         strategy = SequenceMatcherStrategy()
         expected = 0.26537216828478966
         actual = strategy.compare(
             "Aplicações executadas em dispositivos móveis, tipicamente telefones celulares, smartphones, PDAs (Personal Digital Assistant) e tablets",
             "Ambientes de desenvolvimento. Linguagens e bibliotecas. Motores para jogos. Programação de jogos 2D e 3D para dispositivos móveis. Construção de caso: 2D Platform / 2D Puzzle"
+        )
+        
+        assert actual == expected
+
+    def test_compare_real_equivalence_1(self):
+        strategy = SequenceMatcherStrategy()
+        expected = 0.013729977116704805
+        actual = strategy.compare(
+            "Desenvolvimento de algoritmos e estruturas de dados básicas. Desenvolvimento de programação em uma linguagem de alto nível. Metodologia de desenvolvimento de programas: refinamentos sucessivos, modularização e testes básicos",
+            "Paradigmas de programação. Lógica de programação.Introdução ao conceito e uso de algoritmos. Estruturas condicionais e de seleção. Estrutura de repetição. Vetores e matrizes. Formas de representação dos algoritmos"
+        )
+        
+        assert actual == expected
+
+    def test_compare_real_equivalence_2(self):
+        strategy = SequenceMatcherStrategy()
+        expected = 0.012
+        actual = strategy.compare(
+            "Sistemas de Coordenadas Tridimensionais. Superfícies Cilíndricas e Quádricas. Coordenadas Cilíndricas e Esféricas. Funções Vetoriais. Funções de várias Variáveis. Derivadas Parciais. Plano Tangente. Regra da cadeia. Derivada Direcional e Vetor Gradie",
+            "Funções Vetoriais. Funções de várias variáveis. Integrais múltiplas. Derivadas de funções vetoriais. Operadores diferenciais e aplicações. Equações diferenciais. Modelagem. Métodos para solução equações diferenciais ordinárias lineares e não lineares"
         )
         
         assert actual == expected
